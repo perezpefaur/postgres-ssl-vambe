151d758c5c54792d9a7ac420a58e20fc639fbb23
perezpefaur <perezpefaur@uc.cl>
2024-10-12 17:33:36 -0300

    config fix

--- config-wal.sh
+++ config-wal.sh
@@ -3,9 +3,27 @@ set -e
 
 # Function to modify postgresql.conf
 modify_postgresql_conf() {
-    echo "Modifying postgresql.conf to set wal_level to logical"
-    sed -i "s/^#wal_level = .*/wal_level = logical/" "${PGDATA}/postgresql.conf"
-    echo "wal_level has been set to logical in postgresql.conf"
+    echo "Modifying postgresql.conf with required parameters"
+
+    # Set wal_level to 'logical'
+    sed -i "s/^#\?wal_level = .*/wal_level = 'logical'/" "${PGDATA}/postgresql.conf"
+
+    # Set shared_preload_libraries to include pglogical
+    sed -i "s/^#\?shared_preload_libraries = .*/shared_preload_libraries = 'pglogical'/" "${PGDATA}/postgresql.conf"
+
+    # Set wal_sender_timeout to 0
+    sed -i "s/^#\?wal_sender_timeout = .*/wal_sender_timeout = 0/" "${PGDATA}/postgresql.conf"
+
+    # Set max_replication_slots
+    sed -i "s/^#\?max_replication_slots = .*/max_replication_slots = ${MAX_REPLICATION_SLOTS:-10}/" "${PGDATA}/postgresql.conf"
+
+    # Set max_wal_senders
+    sed -i "s/^#\?max_wal_senders = .*/max_wal_senders = ${MAX_WAL_SENDERS:-12}/" "${PGDATA}/postgresql.conf"
+
+    # Set max_worker_processes
+    sed -i "s/^#\?max_worker_processes = .*/max_worker_processes = ${MAX_WORKER_PROCESSES:-10}/" "${PGDATA}/postgresql.conf"
+
+    echo "Parameters have been set in postgresql.conf"
 }
 
 # Check if PGDATA is set
@@ -23,4 +41,4 @@ done
 # Modify postgresql.conf
 modify_postgresql_conf
 
-echo "Configuration complete. PostgreSQL will start with wal_level set to logical."
+echo "Configuration complete. PostgreSQL will start with the required parameters set."
